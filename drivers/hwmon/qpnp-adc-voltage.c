67136bbba2ec50308fdf50be5868353ead549224
jiangshishuang <j00365487@notesmail.huawei.com>
2016-04-19 17:52:34 +0800

    Charger: ambient temperature: Add ambient temperature gathering
    Change-Id: Ib5789b9e4004f5786118f72e297adb74d1e74c0b
    Signed-off-by: jiangshishuang <j00365487@notesmail.huawei.com>

--- drivers/hwmon/qpnp-adc-voltage.c
+++ drivers/hwmon/qpnp-adc-voltage.c
@@ -222,6 +222,7 @@ static struct qpnp_vadc_scale_fn vadc_scale_fn[] = {
 	[SCALE_QRD_SKUC_BATT_THERM] = {qpnp_adc_scale_qrd_skuc_batt_therm},
 	[SCALE_QRD_SKUE_BATT_THERM] = {qpnp_adc_scale_qrd_skue_batt_therm},
 	[SCALE_PMI_CHG_TEMP] = {qpnp_adc_scale_pmi_chg_temp},
+	[SCALE_AMBIENT_THERM] = {qpnp_adc_scale_ambient_therm},
 };
 
 static struct qpnp_vadc_rscale_fn adc_vadc_rscale_fn[] = {
