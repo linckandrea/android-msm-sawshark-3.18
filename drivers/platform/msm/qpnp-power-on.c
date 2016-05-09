45d055bea2c5a6edca4af0cd32e21f64a1ac782a
jiangshishuang <jiangshishuang@huawei.com>
2016-05-09 22:16:04 +0800

    charger: bms: modify report soc
    1.realize automatic shutdown of the cutoff voltage
    2.solve repeated restart when lk trickle enters into kernel
    
    Change-Id: I89c82b2a6e50c9cd34efebb5af722d70fae01581
    Signed-off-by: jiangshishuang <jiangshishuang@huawei.com>

--- drivers/platform/msm/qpnp-power-on.c
+++ drivers/platform/msm/qpnp-power-on.c
@@ -305,6 +305,11 @@ static const char * const qpnp_poff_reason[] = {
 };
 
 /*
+*power_on(usb)
+*/
+int usb_flag;
+
+/*
  * On the kernel command line specify
  * qpnp-power-on.warm_boot=1 to indicate a warm
  * boot of the device.
@@ -2158,6 +2163,8 @@ static int qpnp_pon_probe(struct spmi_device *spmi)
 		return rc;
 	}
 
+	usb_flag = 0;
+
 	index = ffs(pon_sts) - 1;
 	cold_boot = !qpnp_pon_is_warm_reset();
 	if (index >= ARRAY_SIZE(qpnp_pon_reason) || index < 0) {
@@ -2170,6 +2177,8 @@ static int qpnp_pon_probe(struct spmi_device *spmi)
 			"PMIC@SID%d Power-on reason: %s and '%s' boot\n",
 			pon->spmi->sid, qpnp_pon_reason[index],
 			cold_boot ? "cold" : "warm");
+
+		usb_flag = index;
 	}
 
 	/* POFF reason */
