0d92e65678ebdb9c7025c6e4360c78f5866864de
j00365487 <j00365487@notesmail.huawei.com>
2016-09-26 17:51:50 +0800

    power: battery: add node for identify battery manufacturers
    Change-Id: Iefc3efbcf9ac2d938d99f5db3c2dfeec9d8ae6f8
    Signed-off-by: j00365487 <j00365487@notesmail.huawei.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -82,6 +82,9 @@ static ssize_t power_supply_show_property(struct device *dev,
 	};
 	static const char * const typec_pr_text[] = {
 		"none", "dual power role", "sink", "source"
+
+	static char *battery_id[] = {
+		"Unknown", "GUANGYU", "DESAY"
 	};
 	ssize_t ret = 0;
 	struct power_supply *psy = dev_get_drvdata(dev);
@@ -133,6 +136,8 @@ static ssize_t power_supply_show_property(struct device *dev,
 					"%s\n", health_text[value.intval]);
 	else if (off == POWER_SUPPLY_PROP_USB_INPUT_CURRENT)
 		return sprintf(buf, "%d\n", value.intval);
+	else if (off == POWER_SUPPLY_PROP_BATTERY_ID)
+		return sprintf(buf, "%s\n", battery_id[value.intval]);
 	else if (off >= POWER_SUPPLY_PROP_MODEL_NAME)
 		return sprintf(buf, "%s\n", value.strval);
 
@@ -317,6 +322,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(serial_number),
 	POWER_SUPPLY_ATTR(battery_type),
 	POWER_SUPPLY_ATTR(usb_input_current),
+	POWER_SUPPLY_ATTR(battery_id),
 };
 
 static struct attribute *
