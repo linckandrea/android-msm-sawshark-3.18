9a9790120b9a0f5ccce3ced8172029ba65e59fc5
Andrea <linckandrea01@gmail.com>
2023-04-27 06:00:38 +0200

    Revert "ring-buffer: Prevent overflow of size in ring_buffer_resize()"
    This reverts commit 6a13a7862486394e5d5efb94626ca7e76f62c1af.

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -1706,6 +1706,7 @@ int ring_buffer_resize(struct ring_buffer *buffer, unsigned long size,
 		nr_pages = 2;
 
 	size = nr_pages * BUF_PAGE_SIZE;
+
 	/*
 	 * Don't succeed if resizing is disabled, as a reader might be
 	 * manipulating the ring buffer and is expecting a sane state while
