b10e6fe60c33f0ea54770aeab97d83b976073916
Andrea <linckandrea01@gmail.com>
2024-06-24 13:34:57 +0200

    compile: update paths

--- compile_script.sh
+++ compile_script.sh
@@ -3,6 +3,6 @@ export ARCH=arm
 export SUBARCH=arm
 make O=out clean
 make O=out mrproper
-export CROSS_COMPILE=/home/andrea/android/aosp-clang/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/andrea/android/toolchains/aosp-clang/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 make O=out sawshark_defconfig 
-make O=out -j$(nproc --all) 
+make O=out CONFIG_DEBUG_SECTION_MISMATCH=y -j$(nproc --all) 
