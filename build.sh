2ee728410a9ce81dd7075f725bf12827b19b0b1c
linckandrea <linckandrea01@gmail.com>
2024-08-31 00:09:24 +0200

    build.sh: use full defconfig

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ export SUBARCH=arm
 make O=out clean
 make O=out mrproper
 export CROSS_COMPILE=$HOME/Android-dev/toolchains/aosp-clang/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
-make O=out sawshark_defconfig 
+#make O=out sawshark_defconfig 
+make O=out sawshark_full_defconfig
 make O=out -j$(nproc --all) 
