654b2a99c80e7a6b507d24824f37ee1e6a528ca3
linckandrea <linckandrea01@gmail.com>
2025-01-08 13:29:37 +0100

    kernel: cpu: we don't have a perf cluster

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -32,8 +32,8 @@
  * CPU numbers than the big CPUs (e.g., on an 8-core system, CPUs 0-3 would be
  * little and CPUs 4-7 would be big).
  */
-#define LITTLE_CPU_MASK	((1UL << (NR_CPUS / 2)) - 1)
-#define BIG_CPU_MASK	(((1UL << NR_CPUS) - 1) & ~LITTLE_CPU_MASK)
+#define LITTLE_CPU_MASK	(1UL << (NR_CPUS ))
+#define BIG_CPU_MASK	(((1UL << NR_CPUS) - NR_CPUS))
 static const unsigned long little_cluster_cpus = LITTLE_CPU_MASK;
 const struct cpumask *const cpu_lp_mask = to_cpumask(&little_cluster_cpus);
 EXPORT_SYMBOL(cpu_lp_mask);
