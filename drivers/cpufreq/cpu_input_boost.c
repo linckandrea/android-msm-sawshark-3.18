dcc0a2b4f56cf3a43f249361e70a01c29900121d
linckandrea <linckandrea01@gmail.com>
2025-01-10 00:15:51 +0100

    cpu_input_boost: remove input boost

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -19,8 +19,6 @@
 
 struct boost_drv {
 	struct workqueue_struct *wq;
-	struct work_struct input_boost;
-	struct delayed_work input_unboost;
 	struct work_struct max_boost;
 	struct delayed_work max_unboost;
 	struct notifier_block cpu_notif;
@@ -31,14 +29,6 @@ struct boost_drv {
 };
 
 static struct boost_drv *boost_drv_g __read_mostly;
-
-static u32 get_boost_freq(struct boost_drv *b, u32 cpu)
-{
-	if (cpumask_test_cpu(cpu, cpu_lp_mask))
-		return CONFIG_INPUT_BOOST_FREQ_LP;
-
-	return CONFIG_INPUT_BOOST_FREQ_PERF;
-}
 
 static u32 get_boost_state(struct boost_drv *b)
 {
@@ -70,30 +60,11 @@ static void update_online_cpu_policy(void)
 
 static void unboost_all_cpus(struct boost_drv *b)
 {
-	if (!cancel_delayed_work_sync(&b->input_unboost) &&
-	    !cancel_delayed_work_sync(&b->max_unboost))
+	if (!cancel_delayed_work_sync(&b->max_unboost))
 		return;
 
-	clear_boost_bit(b, INPUT_BOOST | WAKE_BOOST | MAX_BOOST);
+	clear_boost_bit(b, WAKE_BOOST | MAX_BOOST);
 	update_online_cpu_policy();
-}
-
-static void __cpu_input_boost_kick(struct boost_drv *b)
-{
-	if (!(get_boost_state(b) & SCREEN_AWAKE))
-		return;
-
-	queue_work(b->wq, &b->input_boost);
-}
-
-void cpu_input_boost_kick(void)
-{
-	struct boost_drv *b = boost_drv_g;
-
-	if (!b)
-		return;
-
-	__cpu_input_boost_kick(b);
 }
 
 static void __cpu_input_boost_kick_max(struct boost_drv *b,
@@ -128,28 +99,6 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	__cpu_input_boost_kick_max(b, duration_ms);
 }
 
-static void input_boost_worker(struct work_struct *work)
-{
-	struct boost_drv *b = container_of(work, typeof(*b), input_boost);
-
-	if (!cancel_delayed_work_sync(&b->input_unboost)) {
-		set_boost_bit(b, INPUT_BOOST);
-		update_online_cpu_policy();
-	}
-
-	queue_delayed_work(b->wq, &b->input_unboost,
-		msecs_to_jiffies(CONFIG_INPUT_BOOST_DURATION_MS));
-}
-
-static void input_unboost_worker(struct work_struct *work)
-{
-	struct boost_drv *b =
-		container_of(to_delayed_work(work), typeof(*b), input_unboost);
-
-	clear_boost_bit(b, INPUT_BOOST);
-	update_online_cpu_policy();
-}
-
 static void max_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), max_boost);
@@ -177,7 +126,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 {
 	struct boost_drv *b = container_of(nb, typeof(*b), cpu_notif);
 	struct cpufreq_policy *policy = data;
-	u32 boost_freq, state;
+	u32 state;
 
 	if (action != CPUFREQ_ADJUST)
 		return NOTIFY_OK;
@@ -194,12 +143,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 	 * Boost to policy->max if the boost frequency is higher. When
 	 * unboosting, set policy->min to the absolute min freq for the CPU.
 	 */
-	if (state & INPUT_BOOST) {
-		boost_freq = get_boost_freq(b, policy->cpu);
-		policy->min = min(policy->max, boost_freq);
-	} else {
 		policy->min = policy->cpuinfo.min_freq;
-	}
 
 	return NOTIFY_OK;
 }
@@ -225,15 +169,6 @@ static int fb_notifier_cb(struct notifier_block *nb,
 	}
 
 	return NOTIFY_OK;
-}
-
-static void cpu_input_boost_input_event(struct input_handle *handle,
-					unsigned int type, unsigned int code,
-					int value)
-{
-	struct boost_drv *b = handle->handler->private;
-
-	__cpu_input_boost_kick(b);
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
@@ -302,7 +237,6 @@ static const struct input_device_id cpu_input_boost_ids[] = {
 };
 
 static struct input_handler cpu_input_boost_input_handler = {
-	.event		= cpu_input_boost_input_event,
 	.connect	= cpu_input_boost_input_connect,
 	.disconnect	= cpu_input_boost_input_disconnect,
 	.name		= "cpu_input_boost_handler",
@@ -325,8 +259,6 @@ static int __init cpu_input_boost_init(void)
 	}
 
 	atomic64_set(&b->max_boost_expires, 0);
-	INIT_WORK(&b->input_boost, input_boost_worker);
-	INIT_DELAYED_WORK(&b->input_unboost, input_unboost_worker);
 	INIT_WORK(&b->max_boost, max_boost_worker);
 	INIT_DELAYED_WORK(&b->max_unboost, max_unboost_worker);
 	atomic_set(&b->state, 0);
